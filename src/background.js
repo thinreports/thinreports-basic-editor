6738307d21db5b9f4b3b725bfa0ef4e4b1bdd5f3
Katsuya Hidaka <hidakatsuya@gmail.com>
2015-01-25 02:08:46 +0900

    Refactor

--- src/background.js
+++ src/background.js
@@ -1,23 +1,16 @@
-/**
- * Listens for the app launching then creates the window
- *
- * @see http://developer.chrome.com/trunk/apps/app.runtime.html
- * @see http://developer.chrome.com/trunk/apps/app.window.html
- */
 chrome.app.runtime.onLaunched.addListener(function() {
-  // Center window on screen.
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
   var width = 800;
   var height = 600;
 
   chrome.app.window.create('app.html', {
-    id: "ThinReportsEditor",
+    id: 'ThinReportsEditor',
     bounds: {
       width: width,
       height: height,
-      left: Math.round((screenWidth-width)/2),
-      top: Math.round((screenHeight-height)/2)
+      left: Math.round((screenWidth　-　width)　/　2),
+      top: Math.round((screenHeight　-　height)　/　2)
     }
   });
 });
