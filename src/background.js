ba6902dac2f75a799865d24e553c45c1c6e4b386
Katsuya Hidaka <hidakatsuya@gmail.com>
2015-01-30 02:16:04 +0900

    Refactor the background.js of ChromeApp

--- src/background.js
+++ src/background.js
@@ -1,16 +1,19 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
-  var width = 800;
-  var height = 600;
+
+  var bounds = { width: 1000, height: 700 };
+  var minBounds = { width: 800, height: 600 };
 
   chrome.app.window.create('app.html', {
-    id: 'ThinreportsEditor',
-    bounds: {
-      width: width,
-      height: height,
-      left: Math.round((screenWidth　-　width)　/　2),
-      top: Math.round((screenHeight　-　height)　/　2)
+    id: 'thinreports-editor',
+    outerBounds: {
+      left: Math.round((screenWidth　-　bounds.width)　/　2),
+      top: Math.round((screenHeight　-　bounds.height)　/　2),
+      width: bounds.width,
+      height: bounds.height,
+      minWidth: minBounds.width,
+      minHeight: minBounds.height
     }
   });
 });
