b33987e584d4dc5d14b0732319d96195ef3cee05
hidakatsuya <hidakatsuya@gmail.com>
2015-01-30 12:28:57 +0900

    Made maximize the window on boot

--- src/background.js
+++ src/background.js
@@ -1,19 +1,14 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  var screenWidth = screen.availWidth;
-  var screenHeight = screen.availHeight;
-
-  var bounds = { width: 1000, height: 700 };
-  var minBounds = { width: 800, height: 600 };
+  var callback = function (win) {
+    win.maximize();
+  };
 
   chrome.app.window.create('app.html', {
     id: 'thinreports-editor',
+    state: 'maximized',
     outerBounds: {
-      left: Math.round((screenWidth　-　bounds.width)　/　2),
-      top: Math.round((screenHeight　-　bounds.height)　/　2),
-      width: bounds.width,
-      height: bounds.height,
-      minWidth: minBounds.width,
-      minHeight: minBounds.height
+      minWidth: 800,
+      minHeight: 600
     }
-  });
+  }, callback);
 });
