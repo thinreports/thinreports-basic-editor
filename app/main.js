aa965a475299dd31b96d42d8471fa2ed4cc9ced8
Katsuya HIDAKA <hidakatsuya@gmail.com>
2017-08-23 03:34:53 +0900

    Available basic text actions on production

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -14,6 +14,26 @@ function createWindow () {
   }))
 
   win.webContents.openDevTools()
+  const template = [
+    {
+      label: 'Application',
+      submenu: [
+        { label: 'Quit', accelerator: 'Command+Q', click: () => { app.quit() } }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+      ]
+    }
+  ]
 
   win.on('closed', () => win = null);
 }
