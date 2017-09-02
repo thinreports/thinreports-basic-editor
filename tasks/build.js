9e27fe21b9f4798a25eff9c582ad134d9f24d112
Katsuya HIDAKA <hidakatsuya@gmail.com>
2017-09-03 03:08:31 +0900

    Now create an archive package on build

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,10 @@
 const where = require('./helper').where
-
 const package = require('../package.json')
+
 const packager = require('electron-packager')
+const archiver = require('archiver')
 const path = require('path')
+const fs = require('fs')
 
 const config = {
   arch: 'x64',
@@ -27,6 +29,7 @@ function build () {
   console.log('\x1b[34mBuilding electron app(s)...\n\x1b[0m')
 
   packager(config)
+    .then(archiveAll)
     .then((appPaths) => {
       console.log('Build(s) successful!')
       console.log(appPaths)
@@ -35,4 +38,33 @@ function build () {
       console.error('\x1b[31mError from `electron-packager` when building app...\x1b[0m')
       console.error(error)
     })
+}
+
+function archiveAll (appPaths) {
+  return Promise.all(appPaths.map(archive))
+}
+
+function archive (path) {
+  const outputPath = `${path}.zip`
+
+  return new Promise((resolve, reject) => {
+    const archive = archiver.create('zip', {})
+    const output = fs.createWriteStream(outputPath)
+
+    output.on('close', () => {
+      resolve(outputPath)
+    })
+
+    archive.on('error', (error) => {
+      reject(error)
+    })
+
+    archive.on('warning', (error) => {
+      reject(error)
+    })
+
+    archive.pipe(output)
+    archive.directory(path, false)
+    archive.finalize()
+  })
 }
