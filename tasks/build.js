c4ea9099f78aa20a746c993eb7b216dd2f171538
Katsuya HIDAKA <hidakatsuya@gmail.com>
2017-09-11 00:07:47 +0900

    Change name of release package

--- tasks/build.js
+++ tasks/build.js
@@ -7,14 +7,21 @@ const path = require('path')
 const fs = require('fs')
 
 const config = {
-  arch: 'x64',
-  asar: true,
-  dir: where.app,
-  icon: path.join(where.app, 'assets/images/app-icon'),
-  ignore: 'editor/',
-  out: path.join(where.root, 'builds'),
-  overwrite: true,
-  platform: ['darwin', 'linux', 'win32']
+  packager: {
+    arch: 'x64',
+    asar: true,
+    dir: where.app,
+    icon: path.join(where.app, 'assets/images/app-icon'),
+    ignore: 'editor/',
+    out: path.join(where.root, 'builds'),
+    overwrite: true,
+    platform: ['darwin', 'linux', 'win32']
+  },
+  packageName: {
+    'Thinreports Editor-darwin-x64': 'ThinreportsEditor-mac',
+    'Thinreports Editor-linux-x64': 'ThinreportsEditor-linux',
+    'Thinreports Editor-win32-x64': 'ThinreportsEditor-windows'
+  }
 }
 
 clean()
@@ -28,7 +35,8 @@ function clean () {
 function build () {
   console.log('\x1b[34mBuilding electron app(s)...\n\x1b[0m')
 
-  packager(config)
+  packager(config.packager)
+    .then(mapPackageNameAll)
     .then(archiveAll)
     .then((appPaths) => {
       console.log('Build(s) successful!')
@@ -38,6 +46,31 @@ function build () {
       console.error('\x1b[31mError from `electron-packager` when building app...\x1b[0m')
       console.error(error)
     })
+}
+
+function mapPackageNameAll (appPaths) {
+  return Promise.all(appPaths.map(mapPackageName))
+}
+
+function mapPackageName (appPath) {
+  return new Promise((resolve, reject) => {
+    const originalName = path.basename(appPath)
+    const mappedName = config.packageName[originalName]
+
+    if (!mappedName) {
+      reject(`No package name setting for #{originalName}`)
+    }
+
+    const mappedAppPath = path.join(path.dirname(appPath), mappedName)
+
+    fs.rename(appPath, mappedAppPath, error => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(mappedAppPath)
+      }
+    })
+  })
 }
 
 function archiveAll (appPaths) {
