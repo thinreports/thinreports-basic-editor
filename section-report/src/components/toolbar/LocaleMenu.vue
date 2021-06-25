71ad4884c5fce51b30e47440964e20974f458259
Richard Barbour <richardbarbour@me.com>
2021-06-25 22:18:07 +0100

    Adds Locale Menu to switch between available languages

---
+++ section-report/src/components/toolbar/LocaleMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option
+        v-for="locale in $i18n.availableLocales"
+        :key="`locale-${locale}`"
+        :value="locale">
+        {{ locale }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+.locale-changer{
+  margin: 10px;
+  position: absolute;
+  right: 10px;
+}
+</style>
