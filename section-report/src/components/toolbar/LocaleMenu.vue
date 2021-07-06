6931feb8f214ff8de40074a209a413a9f06cae0c
Richard Barbour <rbarbour@users.noreply.github.com>
2021-07-06 19:09:00 +0100

    Fixes lint warning
    Co-authored-by: Katsuya HIDAKA <hidakatsuya@users.noreply.github.com>

--- section-report/src/components/toolbar/LocaleMenu.vue
+++ section-report/src/components/toolbar/LocaleMenu.vue
@@ -4,7 +4,8 @@
       <option
         v-for="locale in $i18n.availableLocales"
         :key="`locale-${locale}`"
-        :value="locale">
+        :value="locale"
+      >
         {{ locale }}
       </option>
     </select>
